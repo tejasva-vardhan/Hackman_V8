91e0fb79d7034f058d67d00dd594a925aa7d029c
slashexx <dhruvpuri.35@gmail.com>
2025-10-24 10:28:35 +0530

    fix registration fees
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- frontend/src/components/FAQ.tsx
+++ frontend/src/components/FAQ.tsx
@@ -36,7 +36,7 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer, index }) => {
 const faqData = [
   { id: 1, question: "What is Hackman v8?", answer: "Hackman is a 24-hour Inter-College Hackathon, hosted by the Department of Information Science, DSCE" },
   { id: 2, question: "Who can participate in Hackman v8?", answer: "Any student pursuing engineering from any year with an interest in technology can participate in Hackman. This includes programmers, designers, data scientists, and other tech enthusiasts." },
-  { id: 3, question: "Is there a registration fee for Hackman v8?", answer: "Yes, there is a minimal registration fee of ₹600 per team." },
+  { id: 3, question: "Is there a registration fee for Hackman v8?", answer: "Yes, there is a minimal registration fee of ₹800 per team." },
   { id: 4, question: "What is the theme of Hackman v8?", answer: "Hackman v8 has an open theme for the choice of topics, meaning participants are free to work on any project belonging to a domain of their liking." },
   { id: 5, question: "Will food, shelter and refreshments be provided at Hackman v8?", answer: "Yes. Food, shelter and timely refreshments will be provided to all the participants throughout the duration of the Hackathon." },
   { id: 6, question: "How will projects be judged at Hackman v8?", answer: "Projects at Hackman v8 will be judged based on a set of criteria established by the event organizers, which may include factors such as creativity, technical complexity, feasibility, and potential impact." },
